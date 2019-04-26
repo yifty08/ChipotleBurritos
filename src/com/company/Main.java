7d50e96a766b2ec1f5fd239703650b4c25e6c70a
Yifty <yifty08@icloud.com>
2019-04-26 13:32:35 -0400

    generated 10 different rice for the burritos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,25 +2,45 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+        Random random = new Random();
+
         ArrayList<String> rice = new ArrayList<String>();
         rice.add("white rice");
         rice.add("brown rice");
         rice.add("no rice");
 
-        Collections.sort(rice);
-        System.out.println(rice);
-        Collections.shuffle(rice);
-        System.out.println(rice);
-        System.out.println(rice.get(0));
-
-        for (String r:rice) {
-            System.out.println(r);
+        for (int row = 1; row <= 10; row++){
+            int randomRiceIndex = random.nextInt(rice.size());
+            System.out.println("Burito " + row +
+                    ": " + rice.get(randomRiceIndex));
         }
-        System.out.println(rice);
+
+//        int randomRiceIndex = random.nextInt(rice.size());
+//        System.out.println(rice.get(randomRiceIndex));
+//        System.out.println(rice.get(randomRiceIndex));
+//        System.out.println(rice.get(randomRiceIndex));
+//        System.out.println(rice.get(randomRiceIndex));
+
+//        Collections.sort(rice);
+//        System.out.println(rice);
+//        Collections.shuffle(rice);
+//        System.out.println(rice);
+//        System.out.println(rice.get(0));
+
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("dd");
+//        }
+//        for (String r:rice) {
+//            System.out.println(r);
+//        }
+//        System.out.println(rice);
+//
+
 
 //        int searchIndex = Collections.binarySearch(rice, 0);
 //        if (searchIndex >=0) {
